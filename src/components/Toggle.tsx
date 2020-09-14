837b5493413f0259a93d8c03fb533d6ce9c373be
Will Hackett <12025460+willhackett@users.noreply.github.com>
2020-09-14 12:54:57 +1000

    feat: 🎸 Add toggle state and onChange handler to Toggle comp

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,18 +1,20 @@
-import React, { FC } from "react";
-import { ReactNode } from "react";
-import { styled } from "../shared";
-import Text from "./Text";
-import { CSSObject } from "styled-components";
+import React, { ChangeEventHandler, FC } from 'react';
+import { ReactNode } from 'react';
+import { styled } from '../shared';
+import Text from './Text';
+import { CSSObject } from 'styled-components';
 
 export interface ToggleProps {
   offLabel?: ReactNode;
   onLabel?: ReactNode;
+  checked?: boolean;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
-const Input = styled.input.attrs(() => ({ type: "checkbox" }))(
+const Input = styled.input.attrs(() => ({ type: 'checkbox' }))(
   () =>
     ({
-      position: "absolute",
+      position: 'absolute',
       opacity: 0,
       zIndex: -1,
     } as CSSObject)
@@ -21,39 +23,39 @@ const Input = styled.input.attrs(() => ({ type: "checkbox" }))(
 const Control = styled.label(
   ({ theme: { colours } }) =>
     ({
-      display: "block",
-      width: "2em",
-      height: "1em",
-      position: "relative",
-      pointerEvents: "none",
-      "::before": {
+      display: 'block',
+      width: '2em',
+      height: '1em',
+      position: 'relative',
+      pointerEvents: 'none',
+      '::before': {
         content: '""',
-        position: "absolute",
-        top: "0.05em",
+        position: 'absolute',
+        top: '0.05em',
         left: 0,
-        width: "2.2em",
-        height: "0.9em",
-        borderRadius: "0.5em",
+        width: '2.2em',
+        height: '0.9em',
+        borderRadius: '0.5em',
         background: colours.green,
-        boxShadow: "inset 0 0.1em 0.3em 0.1em rgba(0,0,0,0.3)",
-        pointerEvents: "none",
+        boxShadow: 'inset 0 0.1em 0.3em 0.1em rgba(0,0,0,0.3)',
+        pointerEvents: 'none',
       },
-      "::after": {
+      '::after': {
         content: '""',
-        position: "absolute",
-        top: "-0.15em",
+        position: 'absolute',
+        top: '-0.15em',
         left: 0,
-        width: "0.8em",
-        height: "0.8em",
-        borderRadius: "0.8em",
+        width: '0.8em',
+        height: '0.8em',
+        borderRadius: '0.8em',
         border: `solid 0.23em ${colours.white}`,
-        boxShadow: "0 0.1em 0.2em rgba(0,0,0,0.5)",
+        boxShadow: '0 0.1em 0.2em rgba(0,0,0,0.5)',
         background: colours.darkBlue,
-        transition: "left 150ms ease",
-        pointerEvents: "none",
+        transition: 'left 150ms ease',
+        pointerEvents: 'none',
       },
-      "input:checked~ &::after": {
-        left: "1em",
+      'input:checked~ &::after': {
+        left: '1em',
       },
     } as CSSObject)
 );
@@ -61,22 +63,22 @@ const Control = styled.label(
 const Wrap = styled.label(
   () =>
     ({
-      display: "flex",
-      alignItems: "center",
-      position: "relative",
-      userSelect: "none",
-      cursor: "pointer",
+      display: 'flex',
+      alignItems: 'center',
+      position: 'relative',
+      userSelect: 'none',
+      cursor: 'pointer',
     } as CSSObject)
 );
 
-const Toggle: FC<ToggleProps> = ({ offLabel, onLabel }) => (
+const Toggle: FC<ToggleProps> = ({ offLabel, onLabel, checked, onChange }) => (
   <Wrap>
     {offLabel && (
       <Text mB="0" mR=".5em" lineHeight={1}>
         {offLabel}
       </Text>
     )}
-    <Input />
+    <Input checked={checked} onChange={onChange} />
     <Control />
     {onLabel && (
       <Text mB="0" mL=".7em" lineHeight={1}>
