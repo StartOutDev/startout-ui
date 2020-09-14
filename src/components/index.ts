23def30a6316e971e80c52107e006a2675191790
Will Hackett <12025460+willhackett@users.noreply.github.com>
2020-09-14 16:06:16 +1000

    fix: 🐛 Add TextField export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ export { default as Card } from './Card';
 export { default as Heading } from './Heading';
 export { default as Text } from './Text';
 export { default as TextBox } from './TextBox';
+export { default as TextField } from './TextField';
 export { default as Toggle } from './Toggle';
