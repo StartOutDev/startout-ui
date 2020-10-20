46435a5c351b7ef34c5c23efef5ed4ea79d53aa4
dcdolly <dcdolly@users.noreply.github.com>
2020-10-20 23:53:00 +1030

    TYPE-OF-MESSAGE: added Heading and Description

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Heading from '../components/Heading';
+import Text from '../components/Text';
 import Block from '../components/Block';
 
 const Help = () => (
@@ -8,7 +9,13 @@ const Help = () => (
             <Heading textSize={3.5}>
                 How you can help
             </Heading>
+            <Text maxWidth="475px" m="3rem auto 4.5rem">
+                StartOut encourages you to get involved and help us change lives for good.
+                We need all kinds of support to keep our fantastic programs running.
+                Here are a few ways you can get involved.
+            </Text>
         </Block>
     </Block>
 );
+
 export default Help;
