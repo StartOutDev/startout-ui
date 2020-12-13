97268f20fa4f113ff2d51642895b8e4f8753ca40
dcdolly <dcdolly@users.noreply.github.com>
2020-12-13 16:39:41 +1030

    volunteer in other ways

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -35,6 +35,17 @@ const Help = () => (
                                 fill="#fff" fill-opacity=".2" />
                         </svg>
                     </Block>
+                    <Block background="red" flex="0 0 50%" p="1.5rem .35rem" >
+                        <svg width="560" height="444" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M100.815 142.685V243h42.37V142.685H243.5v-42.37H143.185V0h-42.37v100.315H.5v42.37h100.315z"
+                                fill="#fff" fill-opacity=".2" />
+                        </svg>
+                        <Card flexGrow={2} maxWidth="560">
+                            <Text textSize={0.9} mB="1.5rem">
+                                Corporate support
+                            </Text>
+                        </Card>
+                    </Block>
                 </Block>
             </Block>
         </Block>
