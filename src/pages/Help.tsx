98d034fc1b7b1bf3300c87ca4b65d93232223767
dcdolly <dcdolly@users.noreply.github.com>
2020-12-13 16:28:35 +1030

    added heading and description

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -7,6 +7,14 @@ import Card from '../components/Card';
 const Help = () => (
     <Block background="midLightGrey" textColour="darkBlue">
         <Block textAlign="center" maxWidth="1000px" m="0 auto" p="4rem 2rem">
+            <Heading textSize={3.5}>
+                How you can help
+            </Heading>
+            <Text maxWidth="475px" m="3rem auto 4.5rem">
+                StartOut encourages you to get involved and help us change lives for good.
+                We need all kinds of support to keep our fantastic programs running.
+                Here are a few ways you can get involved.
+            </Text>
         </Block>
     </Block>
 );
