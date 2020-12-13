2957c9fe93e426ffbe77c68d823392918e8afeef
dcdolly <dcdolly@users.noreply.github.com>
2020-12-13 16:37:35 +1030

    added Become a role model

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -15,6 +15,22 @@ const Help = () => (
                 We need all kinds of support to keep our fantastic programs running.
                 Here are a few ways you can get involved.
             </Text>
+
+            <Block display="flex" flexGrow={1} flexWrap="wrap" flexDirection="row">
+                <Block maxWidth="50%" flex="0 0 50%" flexDirection="column">
+                    <Block background="orange" flex="0 0 50%" p="1.5rem .35rem">
+                        <svg width="661" height="340" viewBox="0 0 661 525" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M31 31C31 28.2386 33.2386 26 36 26H586C588.761 26 591 28.2386 591 31V411H31V31Z"
+                                fill="url(#pattern0)" />
+                        </svg>
+                        <Card flexGrow={2} maxWidth="560">
+                            <Text textSize={0.9} mB="1.5rem">
+                                Become a role model
+                            </Text>
+                        </Card>
+                    </Block>
+                </Block>
+            </Block>
         </Block>
     </Block>
 );
